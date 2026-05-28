ae1cc2491662209d97e2d11a5aad88c7f376783b
MesnerH <hubertmissar04@gmail.com>
2026-05-28 17:54:21 +0200

    add docstrings

--- utils.py
+++ utils.py
@@ -1,11 +1,21 @@
-def add (a: int, b: int) -> int:
+"""kalkulator"""
+
+
+def add(a: int, b: int) -> int:
+    """dodawanie"""
     return a + b
 
-def subtract (a: int, b: int) -> int:
+
+def subtract(a: int, b: int) -> int:
+    """odejmowanie"""
     return a - b
 
-def multiply (a: int, b: int) -> int:
+
+def multiply(a: int, b: int) -> int:
+    """mnozenie"""
     return a * b
 
-def divide (a: int, b: int) -> float:
+
+def divide(a: int, b: int) -> float:
+    """dzielenie"""
     return a / b
