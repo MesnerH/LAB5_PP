5a2c7bdcec3c2f9de092f4c0d231c375b7060422
MesnerH <hubertmissar04@gmail.com>
2026-05-28 18:11:43 +0200

    add converting numbers decimal to binary

--- utils.py
+++ utils.py
@@ -19,3 +19,12 @@ def multiply(a: int, b: int) -> int:
 def divide(a: int, b: int) -> float:
     """dzielenie"""
     return a / b
+
+
+def to_binary(n: int) -> str:
+    """konwertowanie liczby naturalnej do binarnej"""
+    if not isinstance(n, int) or isinstance(n, bool):
+        raise TypeError("Argument musi byc liczba naturalna")
+    if n < 0 or n > 100:
+        raise ValueError("Argument ma być w zakresie od [0 do 100]")
+    return bin(n)
